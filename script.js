ed4b27f56c20952b97f8630a23c52f1a7a2ed78f
sushil174 <sushilkoregave123@gmail.com>
2024-09-24 01:37:17 +0530

    add dark and light mode

---
+++ script.js
@@ -0,0 +1,38 @@
+// check for saved 'darkMode' in localStorage
+let lightMode = localStorage.getItem('lightMode'); 
+
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+const enableLightMode = () => {
+  // 1. Add the class to the body
+  document.body.classList.add('lightmode');
+  // 2. Update darkMode in localStorage
+  localStorage.setItem('lightMode', 'enabled');
+}
+
+const disableLightMode = () => {
+  // 1. Remove the class from the body
+  document.body.classList.remove('lightmode');
+  // 2. Update darkMode in localStorage 
+  localStorage.setItem('lightMode', null);
+}
+ 
+// If the user already visited and enabled darkMode
+// start things off with it on
+if (lightMode === 'enabled') {
+    enableLightMode();
+}
+
+// When someone clicks the button
+darkModeToggle.addEventListener('click', () => {
+  // get their darkMode setting
+  lightMode = localStorage.getItem('lightMode'); 
+  
+  // if it not current enabled, enable it
+  if (lightMode !== 'enabled') {
+    enableLightMode();
+  // if it has been enabled, turn it off  
+  } else {  
+    disableLightMode(); 
+  }
+});
